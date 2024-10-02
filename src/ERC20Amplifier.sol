de2c0e30b1fecf4d3406cd007a3f7fbd084220a2
Yak <y@kuhi.to>
2024-10-02 18:37:03 -0300

    add ERC20Amplifier source code

---
+++ src/ERC20Amplifier.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+/* yak tracks all over the place */
+pragma solidity 0.8.23;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+event Deposit(address indexed sender, uint256 originalTokenAmount);
+event Withdraw(address indexed receiver, uint256 originalTokenAmount);
+
+contract ERC20Amplifier is ERC20 {
+    address public originalToken;
+    uint256 public amplification;
+
+    constructor(address _originalToken, uint256 _amplification, string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+    {
+        originalToken = _originalToken;
+        amplification = _amplification;
+    }
+    
+    function deposit(uint256 _originalTokenAmount) public {
+        require(_originalTokenAmount > 0, "!gt0");
+
+        _mint(msg.sender, _originalTokenAmount * amplification);
+        IERC20(originalToken).transferFrom(msg.sender, address(this), _originalTokenAmount);
+
+        emit Deposit(msg.sender, _originalTokenAmount);
+    }
+
+    function withdraw(uint256 _originalTokenAmount) external {
+        require(_originalTokenAmount > 0, "!gt0");
+
+        _burn(msg.sender, _originalTokenAmount * amplification);
+        IERC20(originalToken).transfer(msg.sender, _originalTokenAmount);
+
+        emit Withdraw(msg.sender, _originalTokenAmount);
+    }
+}
